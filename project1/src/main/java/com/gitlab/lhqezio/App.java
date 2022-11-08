901e832e3753fe5b50170665718625116702eeac
FuPeiJiang <42662615+FuPeiJiang@users.noreply.github.com>
2022-11-08 16:26:02 -0500

    computers.csv: has a null field, switch "Computer" for 13, "Laptop" for 12 fields

--- project1/src/main/java/com/gitlab/lhqezio/App.java
+++ project1/src/main/java/com/gitlab/lhqezio/App.java
@@ -5,7 +5,11 @@ import com.gitlab.lhqezio.cli.InvalidUserInputException;
 import com.gitlab.lhqezio.items.Computer;
 import com.gitlab.lhqezio.items.Laptop;
 import com.gitlab.lhqezio.items.Product;
+import com.gitlab.lhqezio.util.CSV_Util;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -14,64 +18,94 @@ import java.util.Scanner;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
         Display display = new Display();
         boolean cont = true;
         display.welcomeMsg();
-        if(display.loginMsg(new User())){
-            while(cont){
-                List <Product> prod = loadProducts();
+        if (display.loginMsg(new User())) {
+            while (cont) {
+                List<Product> prod = loadProducts();
                 Scanner sc = new Scanner(System.in);
                 char selection = 'a';
                 display.homepage(prod);
-                while(cont){
+                while (cont) {
                     selection = sc.next().charAt(0);
                     sc.nextLine();
-                    try{
+                    try {
                         switch (selection) {
                             case '1':
                                 display.homepage(prod);
                                 break;
                             case '2':
-                                switch(display.searchFilterPage()){
-                                    case '1':case '2':case '3':case '4':
-                                    String keyword = sc.nextLine();
-                                    display.searchResult(prod, keyword);
-                                    break;
+                                switch (display.searchFilterPage()) {
+                                    case '1':
+                                    case '2':
+                                    case '3':
+                                    case '4':
+                                        String keyword = sc.nextLine();
+                                        display.searchResult(prod, keyword);
+                                        break;
                                     case '5':
-                                    display.homepage(prod);
-                                    break;
+                                        display.homepage(prod);
+                                        break;
                                     default:
-                                    throw new InvalidUserInputException(null);
+                                        throw new InvalidUserInputException(null);
                                 }
                             case '4':
                                 return;
                             default:
                                 throw new InvalidUserInputException(null);
                         }
-                    }
-                    catch(InvalidUserInputException e ){
+                    } catch (InvalidUserInputException e) {
                         System.out.println("Invalid input go back to homepage");
                         display.homepage(prod);
                     }
 
                 }
             }
-        }
-        else{
+        } else {
             return;
-        };
+        }
 
     }
-    public static List<Product> loadProducts(){
-        List<Product>products = new ArrayList<>();
-        products.add(new Laptop("Lenovo IdeaPad 5X","Lenovo",1000,10,"Thin And Light","Leno01","Intel Core i7","16GB","1TB","Nvidia GTX 1050","Windows 10",500,18));
-        products.add(new Laptop("Lenovo X1 Carbon","Lenovo",1000,10,"Thin And Ultra Light","Leno02","Intel Core i7","16GB","1TB","Nvidia GTX 1050","Windows 10",500,18));
-        products.add(new Computer("Alienware X51","Lenovo",1000,10,"Thin And Light","Leno01","Intel Core i7","16GB","1TB","Nvidia GTX 1050","Windows 10",500));
-        return products;
+
+    public static File getGitIgnoreDir() {
+        File dir_ = new File(System.getProperty("user.dir"));
+        while (!(new File(dir_, ".gitignore")).exists()) {
+            dir_ = dir_.getParentFile();
+        }
+        return dir_;
+    }
+
+    public static List<Product> loadProducts() {
+
+        Path csvPath = (new File(getGitIgnoreDir(), "computers.csv")).toPath();
+        try {
+            List<Product> products = new ArrayList<>();
+
+            String[][] allRowsArr = CSV_Util.parseCSV(CSV_Util.readBytesAdd2Newline(csvPath));
+            for (int i = 0; i < allRowsArr.length; i++) {
+                String[] rowArr = allRowsArr[i];
+                switch (rowArr[0]) {
+                    case "Laptop":
+                        products.add(new Laptop(rowArr[1], rowArr[2], Double.parseDouble(rowArr[3]), Integer.parseInt(rowArr[4]), rowArr[5], rowArr[6], rowArr[7], rowArr[8], rowArr[9], rowArr[10], rowArr[11], Integer.parseInt(rowArr[12]), Integer.parseInt(rowArr[13])));
+                        break;
+                    case "Computer":
+                        products.add(new Computer(rowArr[1], rowArr[2], Double.parseDouble(rowArr[3]), Integer.parseInt(rowArr[4]), rowArr[5], rowArr[6], rowArr[7], rowArr[8], rowArr[9], rowArr[10], rowArr[11], Integer.parseInt(rowArr[12])));
+                        break;
+                }
+            }
+
+            return products;
+        } catch (IOException e) {
+            throw new IllegalArgumentException(e);
+        }
+
+        // List<Product>products = new ArrayList<>();
+        // products.add(new Laptop("Lenovo IdeaPad 5X","Lenovo",1000,10,"Thin And Light","Leno01","Intel Core i7","16GB","1TB","Nvidia GTX 1050","Windows 10",500,18));
+        // products.add(new Laptop("Lenovo X1 Carbon","Lenovo",1000,10,"Thin And Ultra Light","Leno02","Intel Core i7","16GB","1TB","Nvidia GTX 1050","Windows 10",500,18));
+        // products.add(new Computer("Alienware X51","Lenovo",1000,10,"Thin And Light","Leno01","Intel Core i7","16GB","1TB","Nvidia GTX 1050","Windows 10",500));
+        // return products;
     }
 };
-
---
+++ project1/src/main/java/com/gitlab/lhqezio/util/CSV_Util.java
@@ -0,0 +1,206 @@
+package com.gitlab.lhqezio.util;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SeekableByteChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class CSV_Util {
+
+    public static String[][] parseCSV(byte[] buf) {
+        // get length
+        int colCount = 0;
+
+        int c_ = 0;
+        byte char_ = buf[c_];
+        outer: while (true) {
+            switch (char_) {
+                case '"':
+                    c_++;
+                    char_ = buf[c_];
+                    while (true) {
+                        switch (char_) {
+                            case '"':
+                                byte nextChar = buf[c_ + 1];
+                                if (nextChar == '"') {
+                                    c_ += 2;
+                                    char_ = buf[c_];
+                                    continue;
+                                } else {
+                                    colCount++;
+                                    if (nextChar == '\n') {
+                                        break outer;
+                                    }
+                                    c_ += 2;
+                                    char_ = buf[c_];
+                                    continue outer;
+                                }
+                        }
+                        c_++;
+                        char_ = buf[c_];
+                    }
+                default:
+                    outer2: while (true) {
+                        switch (char_) {
+                            case '\n':
+                                colCount++;
+                                c_++;
+                                char_ = buf[c_];
+
+                                break outer;
+                            case ',':
+                                colCount++;
+                                c_++;
+                                char_ = buf[c_];
+                                break outer2;
+                        }
+                        c_++;
+                        char_ = buf[c_];
+                    }
+            }
+        }
+
+        int linesCount = 2;
+        while (c_ < buf.length) {
+            if (buf[c_] == '\n') {
+                linesCount++;
+            }
+            c_++;
+        }
+        int trailingNewlines = 2;
+        c_ = buf.length - 3;
+        while (buf[c_] == '\n') {
+            trailingNewlines++;
+            c_--;
+        }
+        int rowCount = linesCount - trailingNewlines;
+
+        System.out.println(colCount);
+        System.out.println(rowCount);
+
+        String[][] allRowsArr = new String[rowCount][];
+        String[] rowArr = new String[colCount];
+        allRowsArr[0] = rowArr;
+        int rowsIdx = 1;
+
+        int fieldIdx = 0;
+
+        try {
+            c_ = 0;
+            int cBak;
+            outer: while (true) {
+                switch (char_) {
+                    case '"':
+                        c_++;
+                        cBak = c_;
+                        char_ = buf[c_];
+                        while (true) {
+                            switch (char_) {
+                                case '"':
+                                    byte nextChar = buf[c_ + 1];
+                                    if (nextChar == '"') {
+                                        c_ += 2;
+                                        char_ = buf[c_];
+                                        continue;
+                                    } else {
+                                        String field_ = new String(buf, cBak, c_ - cBak, "UTF-8");
+                                        String escapedField = field_.replace("\"\"", "\"");
+                                        rowArr[fieldIdx] = escapedField;
+                                        fieldIdx++;
+
+                                        c_ += 2;
+                                        if (nextChar == '\n') {
+                                            if (buf[c_] == '\n') {
+                                                break outer;
+                                            } else {
+                                                rowArr = new String[colCount];
+                                                fieldIdx = 0;
+                                                allRowsArr[rowsIdx] = rowArr;
+                                                rowsIdx++;
+                                            }
+                                        }
+                                        char_ = buf[c_];
+                                        continue outer;
+                                    }
+                            }
+                            c_++;
+                            char_ = buf[c_];
+                        }
+                    default:
+                        cBak = c_;
+                        outer2: while (true) {
+                            switch (char_) {
+                                case '\n': {
+                                    String field_ = new String(buf, cBak, c_ - cBak, "UTF-8");
+                                    rowArr[fieldIdx] = field_;
+                                    fieldIdx++;
+
+                                    c_++;
+                                    char_ = buf[c_];
+
+                                    if (char_ == '\n') {
+                                        break outer;
+                                    }
+                                    rowArr = new String[colCount];
+                                    fieldIdx = 0;
+                                    allRowsArr[rowsIdx] = rowArr;
+                                    rowsIdx++;
+                                    break outer2;
+                                }
+                                case ',': {
+                                    String field_ = new String(buf, cBak, c_ - cBak, "UTF-8");
+                                    rowArr[fieldIdx] = field_;
+                                    fieldIdx++;
+
+                                    c_++;
+                                    char_ = buf[c_];
+                                    break outer2;
+                                }
+                            }
+                            c_++;
+                            char_ = buf[c_];
+                        }
+                }
+            }
+            return allRowsArr;
+        } catch (Exception e) {
+            System.out.println(e);
+            return new String[][] {};
+        }
+
+    }
+
+    public static byte[] readBytesAdd2Newline(Path path) throws IOException {
+        try (SeekableByteChannel sbc = Files.newByteChannel(path)) {
+            ByteBuffer buffer = ByteBuffer.allocateDirect((int) (sbc.size()) + 2);
+            sbc.read(buffer);
+            byte[] arr = new byte[buffer.capacity()];
+            // System.out.println(arr.length);
+            buffer.position(0);
+            buffer.get(arr);
+            arr[arr.length - 1] = '\n';
+            arr[arr.length - 2] = '\n';
+            // System.out.println(new String(arr, StandardCharsets.UTF_8));
+            return arr;
+        }
+    }
+
+    public static byte[] stringBytesAdd2Newline(String str_) {
+        try {
+            byte[] from = str_.getBytes("UTF-8");
+            byte[] arr = new byte[from.length + 2];
+            for (int i = 0; i < from.length; i++) {
+                arr[i] = from[i];
+            }
+            arr[arr.length - 1] = '\n';
+            arr[arr.length - 2] = '\n';
+            return arr;
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println(e);
+            return new byte[0];
+        }
+    }
+
+}
